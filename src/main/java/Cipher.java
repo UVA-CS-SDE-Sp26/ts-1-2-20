d0540e12e20dd7cdf7d572396f4bfe8abef251a0
raymondchen10 <646raymond@gmail.com>
2026-02-13 01:03:29 -0500

    finalized cipher and completed ciphertest

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -2,11 +2,10 @@ public class Cipher
 {
     private String referenceKey;
     private String cipherKey;
-    private Cipher cipher;
 
     public Cipher(String referenceKey, String cipherKey)
     {
-        validate(referenceKey, cipherKey);
+        validate(referenceKey, cipherKey);      //check first if reference and cipher keys are valid
 
         this.referenceKey = referenceKey;
         this.cipherKey = cipherKey;
@@ -14,27 +13,27 @@ public class Cipher
 
     public String decipher(String encryptedText)
     {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();     //create a mutable string, more efficient than normal String class
 
-        for(int i = 0; i < encryptedText.length(); i++)
+        for(int i = 0; i < encryptedText.length(); i++)     //loop through each char in the encrypted text
         {
-            char encryptedChar = encryptedText.charAt(i);
+            char encryptedChar = encryptedText.charAt(i);   //save each encrypted char
 
-            int index = cipherKey.indexOf(encryptedChar);
+            int index = cipherKey.indexOf(encryptedChar);   //find index of encrytedChar in the cipher key
 
-            if(index != -1)
+            if(index != -1)         //if found, match the index to the reference key
             {
                 char plainChar = referenceKey.charAt(index);
-                result.append(plainChar);
+                result.append(plainChar);       //add to result string
             }
-            else {
+            else {          //if not found, probably whitespace so add it
                 result.append(encryptedChar);
             }
         }
-        return result.toString();
+        return result.toString();   //return result
     }
 
-    private void validate(String reference, String key)
+    private void validate(String reference, String key)     //make sure reference and cipher keys are valid
     {
         //check if null
         if (reference == null || key == null) {
@@ -45,6 +44,7 @@ public class Cipher
         {
             throw new IllegalArgumentException("Key lines must have the same length");
         }
+        //check if duplicate chars
         for(int i = 0; i < key.length(); i++)
         {
             char c = key.charAt(i);
@@ -53,5 +53,13 @@ public class Cipher
                 throw new IllegalArgumentException("Duplicate character in key" + c);
             }
         }
+    }
+
+    public String getReferenceKey() {
+        return referenceKey;
+    }
+
+    public String getCipherKey() {
+        return cipherKey;
     }
 }
