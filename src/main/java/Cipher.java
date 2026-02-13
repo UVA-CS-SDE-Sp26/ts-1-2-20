38e4a2946d9796a67b0f262803ef2b595ea87ca3
raymondchen10 <646raymond@gmail.com>
2026-02-12 19:38:47 -0500

    finished cipher

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,13 +1,56 @@
 public class Cipher
 {
     private String referenceKey;
-    private String cipherText;
+    private String cipherKey;
 
-    public Cipher(String referenceKey, String cipherText)
+    public Cipher(String referenceKey, String cipherKey)
     {
+
+
         this.referenceKey = referenceKey;
-        this.cipherText = cipherText;
+        this.cipherKey = cipherKey;
+    }
 
+    public String decipher(String encryptedText)
+    {
+        StringBuilder result = new StringBuilder();
 
+        for(int i = 0; i < encryptedText.length(); i++)
+        {
+            char encryptedChar = encryptedText.charAt(i);
+
+            int index = cipherKey.indexOf(encryptedChar);
+
+            if(index != -1)
+            {
+                char plainChar = referenceKey.charAt(index);
+                result.append(plainChar);
+            }
+            else {
+                result.append(encryptedChar);
+            }
+        }
+        return result.toString();
+    }
+
+    private void validate(String reference, String key)
+    {
+        //check if null
+        if (reference == null || key == null) {
+            throw new IllegalArgumentException("Key lines cannot be null");
+        }
+        //check if lengths are same
+        if(reference.length() != key.length())
+        {
+            throw new IllegalArgumentException("Key lines must have the same length");
+        }
+        for(int i = 0; i < key.length(); i++)
+        {
+            char c = key.charAt(i);
+            if(key.indexOf(c) != key.lastIndexOf(c))
+            {
+                throw new IllegalArgumentException("Duplicate character in key" + c);
+            }
+        }
     }
 }
