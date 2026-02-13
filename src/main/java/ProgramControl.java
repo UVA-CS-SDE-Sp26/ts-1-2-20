ca7c2ac58167acda89c11acc22d575418c2e976b
mjaldi <mjaldi1919@gmail.com>
2026-02-12 20:06:46 -0500

    added part c doc

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -1,9 +1,27 @@
 import java.util.logging.FileHandler;
 
+
+
 public class ProgramControl {
         private FileHandler fileHandler;
 
         public ProgramControl(FileHandler fileHandler) {
+                this.fileHandler = fileHandler;
+
+        }
+
+        public void displayFileMenu(){
+                System.out.println("Available Files:");
+                List<String> files = fileHandler.listFiles();
+
+                if (files.isEmpty()) {
+                        System.out.println("No Files Found");
+                        return;
+                }
+
+                for (int i=)
+
+
 
         }
 }
