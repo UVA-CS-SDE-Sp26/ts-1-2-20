89dfe2ce06ab5f356a4a4cba5ea73d183e1340c6
acx29 <mikedornic@gmail.com>
2026-02-13 02:00:04 -0500

    oeirngsoiengt

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class FileHandlerTest {
 
@@ -8,6 +8,13 @@ public class FileHandlerTest {
         FileHandler fh = new FileHandler();
         Assertions.assertNotNull(fh.getListOfFiles());
     }
+
+    @Test
+    void fileDoesNotExistReturnsNull() {
+        FileHandler fh = new FileHandler();
+        Assertions.assertNull(fh.fileHandler(0, "thisFileDoesNotExist.txt"));
+    }
+
 }
 
                                 
