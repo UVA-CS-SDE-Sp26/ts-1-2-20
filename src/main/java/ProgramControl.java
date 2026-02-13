f47d0c7ecf288eb0404aac1c2b8fba388c51e5cc
mjaldi <mjaldi1919@gmail.com>
2026-02-12 20:14:13 -0500

    Program control class

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -19,9 +19,18 @@ public class ProgramControl {
                         return;
                 }
 
-                for (int i=)
+                for (int i=0;files.size();i++){
+                        System.out.prinln((i+1)+"."+files.get(i));
+                }
 
 
+
+        }
+        public void displayFileContents(String fileName){
+                System.out.println("Reading file: "+fileName);
+
+                String contents = fileHandler.readFile(fileName);
+                System.out.println(contents);
 
         }
 }
