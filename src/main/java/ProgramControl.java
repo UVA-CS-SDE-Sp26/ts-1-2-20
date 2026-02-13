268b4c5a3e8933a9974a9169812b30a5bc53c834
mjaldi <mjaldi1919@gmail.com>
2026-02-12 19:44:08 -0500

    added part c doc

---
+++ src/main/java/ProgramControl.java
@@ -0,0 +1,9 @@
+import java.util.logging.FileHandler;
+
+public class ProgramControl {
+        private FileHandler fileHandler;
+
+        public ProgramControl(FileHandler fileHandler) {
+
+        }
+}
